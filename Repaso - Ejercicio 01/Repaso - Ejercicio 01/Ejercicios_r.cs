75491bdb630b56d9b872ac3abbfb9b160498d1f6
gutipy <aguus.sxe.cs@hotmail.com>
2022-03-24 17:55:53 -0300

    Repaso - Ejercicio 01
    Adjunto el ejercicio 01 de las consignas de Repaso de Edmodo.

---
+++ Repaso - Ejercicio 01/Repaso - Ejercicio 01/Ejercicios_r.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repaso___Ejercicio_01
+{
+    internal class Ejercicios_r
+    {
+        public void G1E01_r()
+        {
+            //Declaro variables
+            int ultimos_tres_digitos;
+            int contador = 0;
+            string resultado = "";
+
+            //Pido al usuario que ingrese sus últimos tres dígitos del registro
+            Console.WriteLine("Ingrese sus últimos tres números del registro");
+            ultimos_tres_digitos = Convert.ToInt32(Console.ReadLine());
+
+            //Validación por si los ultimos tres dígitos son menores a 100
+            if(ultimos_tres_digitos < 100)
+            {
+                ultimos_tres_digitos = ultimos_tres_digitos + 100;
+            }
+
+            //Bucle para mostrar en pantalla todos los números hasta llegar al´número ingresado por el user
+            while (contador != ultimos_tres_digitos)
+            {
+                contador++;
+
+                if(contador % 3 == 0 && contador % 5 != 0)
+                {
+                    resultado = contador.ToString() + " - " + " Foo";
+                }
+                else if(contador % 5 == 0 && contador % 3 != 0)
+                {
+                    resultado = contador.ToString() + " - " + " Bar";
+                }
+                else if(contador % 3 == 0 && contador % 5 == 0)
+                {
+                    resultado = contador.ToString() + " - " + " FooBar";
+                }
+                else
+                {
+                    resultado = contador.ToString();
+                }
+
+                Console.WriteLine(resultado);
+                
+
+            }
+
+        }
+    }
+}
---
+++ Repaso - Ejercicio 01/Repaso - Ejercicio 01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repaso___Ejercicio_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ejercicios_r E = new Ejercicios_r();
+
+            E.G1E01_r();
+
+            Console.WriteLine("Ingrese enter para salir...");
+            Console.ReadLine();
+        }
+    }
+}
